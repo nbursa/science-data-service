07c2ce71db2dde369e93ab8493631f6f9e960ebf
Nenad <nbursa@gmail.com>
2024-06-07 12:15:05 +0200

    Config improve

--- scraping/science_scraper/settings.py
+++ scraping/science_scraper/settings.py
@@ -1,10 +1,10 @@
 BOT_NAME = 'science_scraper'
 
-SPIDER_MODULES = ['science_scraper.spiders']
-NEWSPIDER_MODULE = 'science_scraper.spiders'
+SPIDER_MODULES = ['scraping.science_scraper.spiders']
+NEWSPIDER_MODULE = 'scraping.science_scraper.spiders'
 
 ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
-    'science_scraper.pipelines.MongoPipeline': 300,
+    'scraping.science_scraper.pipelines.MongoPipeline': 300,
 }
---
+++ test_imports.py
@@ -0,0 +1,3 @@
+from scraping.science_scraper.spiders.science_spider import ScienceSpider
+
+print("Import successful!")
