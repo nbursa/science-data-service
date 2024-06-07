a7eea7044b1e466f3c856a2fdfee39bbbf9caddd
Nenad <nbursa@gmail.com>
2024-06-07 12:12:36 +0200

    Initialize celery tasks

--- scraping/celery_tasks.py
+++ scraping/celery_tasks.py
@@ -1,12 +1,25 @@
+import logging
 from celery import Celery
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-# from science_scraper.spiders.science_spider import ScienceSpider
 from app.services.translation import translate_content
 from app.models.article import Article
 from scraping.science_scraper.spiders.science_spider import ScienceSpider
+from app.config import settings
 
-app = Celery('tasks', broker='redis://localhost:6379/0')
+app = Celery('tasks', broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_RESULT_BACKEND)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='Europe/Belgrade',
+    enable_utc=True,
+)
 
 @app.task
 def run_spider():
