ab78a2d636a033fa5457e7c076a622a0891d56e8
Nenad <nbursa@gmail.com>
2024-06-07 12:13:00 +0200

    Config celery

--- scraping/celeryconfig.py
+++ scraping/celeryconfig.py
@@ -1,19 +1,21 @@
-from celery import Celery
-from app.config import settings
-
-celery = Celery(
-    'tasks',
-    broker=settings.CELERY_BROKER_URL,
-    backend=settings.CELERY_RESULT_BACKEND
-)
-
-celery.conf.update(
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-    timezone='Europe/Belgrade',
-    enable_utc=True,
-)
+from celery.schedules import crontab
 
 broker_url = 'redis://localhost:6379/0'
 result_backend = 'redis://localhost:6379/0'
+
+task_serializer = 'json'
+accept_content = ['json']
+result_serializer = 'json'
+timezone = 'Europe/Belgrade'
+enable_utc = True
+
+beat_schedule = {
+    'run-spider-every-minute': {
+        'task': 'scraping.celery_tasks.run_spider',
+        'schedule': crontab(minute='*/1'),  # every minute
+    },
+    'translate-articles-every-minute': {
+        'task': 'scraping.celery_tasks.translate_articles',
+        'schedule': crontab(minute='*/1'),  # every minute
+    },
+}
